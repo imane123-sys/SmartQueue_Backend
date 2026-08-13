f7918eec4b774f02388c206692803f74dd5c3168
Imane Ramadane <ramadaneimane@gmail.com>
2026-08-13 12:06:31 +0100

    création de projet avec les  dépendances nécessaires pour le développement

---
+++ src/main/java/org/example/smartqueue/SmartQueueApplication.java
@@ -0,0 +1,13 @@
+package org.example.smartqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartQueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/smartqueue/SmartQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
